b4ce499e7b1547fcf84893cd0e8d6fc16db735ce
harrysong961205 <fkdlzldeja@gmail.com>
2023-03-30 03:03:06 +0900

    Add 배경, 하단 네비게이션
    배경 : 아놀드(AN)
    하단 네비게이션 추가(기능추가 예정)

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
+import Navigation from './Navigation';
+
 
 function App() {
+
+  let [운동,운동변경] = useState("가슴 루틴 추천");
+
+  const styleLetter = {
+    color: 'white', // 글자색을 하얀색으로 설정
+  };
+
+  const style = {
+    backgroundImage: 'url("AN.jpg")',
+    backgroundSize: 'cover',
+    height: '100vh',
+    position: 'relative',
+  };
+  const overlay = {
+    content: '',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)', 
+    backdropFilter: 'blur(5px)', 
+  };
+
+  let welcome = "어세오세요 지옥에!";
+
+  function func(){
+    return "go to the f gym"
+  };
+
+  function nav() {
+    return (
+      <div>
+        <Navigation />
+      </div>
+    );
+  }
+
   return (
+    <div style = {style}>
+    <div style = {overlay}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="head">
+        <div>welcome to hellchang</div>
+      </div>
+      <div className="list">
+        <h3 style = {styleLetter}>{운동}</h3>
+      </div>
+      <div className="list">
+        <h3 style = {styleLetter}>{운동}<span>😀</span></h3>
+      </div >
+      <div className="list">
+        <h3 style = {styleLetter}>{운동}</h3>
+      </div>
+      <h4 style = {styleLetter}>{welcome}
+        </h4>
+      <h2>{nav()}</h2>
+    </div>
+    </div>
     </div>
   );
 }
-
 export default App;
---
+++ blog/src/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#">Routine</a></li>
+        <li><a href="#">콜로세움</a></li>
+        <li><a href="#">my page</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
